12d155134983737a42782d254a862aea35d83066
UnaibNaeem <Unaibnaeem913@gmail.com>
2025-02-25 23:04:56 +0500

    Added environment variables

---
+++ frontend/src/environments/environment.production.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiURL: 'https://ng-ecomm-store-production.up.railway.app',
+  stripePublicKey:
+    'pk_test_51QtuJlHqpQQqeNCr7xIXZ7YYsSfbONb3JIrb5DYrciaX4mElDcj0O3M3ozzCTJKxCTaFck9G9BVdofhj5aRGGBi500BiAXCfvu',
+};
