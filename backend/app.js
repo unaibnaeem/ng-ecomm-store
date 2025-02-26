15381ffa141293f04ddbfee582c5e0bbe6b98b82
UnaibNaeem <Unaibnaeem913@gmail.com>
2025-02-26 13:07:30 +0500

    Resolved cors error

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:4200",
+    origin: ["http://localhost:4200", "https://ng-ecomm-store.vercel.app"],
     credentials: true,
   })
 );
